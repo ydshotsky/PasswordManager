df94971afcc66f382e68c12229e0a951a3e0c167
Harshit Khanna <harshitkhanna006@gmail.com>
2025-12-28 18:31:59 +0530

    fixed applcation prod name

--- src/main/java/com/passwordManager/PasswordManagerApplication.java
+++ src/main/java/com/passwordManager/PasswordManagerApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PasswordManagerApplication{
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(PasswordManagerApplication.class);
-
-        app.addListeners(event ->
-                System.out.println("SPRING_DATASOURCE_URL=" + System.getenv("SPRING_DATASOURCE_URL"))
-        );
-        app.run(args);
+        SpringApplication.run(PasswordManagerApplication.class, args);
     }
 }
