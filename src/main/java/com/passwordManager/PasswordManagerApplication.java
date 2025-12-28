635fae2e0c61cee6aee7c48d84249f3afda7654f
Harshit Khanna <harshitkhanna006@gmail.com>
2025-12-28 17:59:34 +0530

    refactored main class

--- src/main/java/com/passwordManager/PasswordManagerApplication.java
+++ src/main/java/com/passwordManager/PasswordManagerApplication.java
@@ -1,19 +1,16 @@
 package com.passwordManager;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PasswordManagerApplication{
     public static void main(String[] args) {
-        SpringApplication.run(PasswordManagerApplication.class, args);
-    }
-    @PostConstruct
-public void logEnv() {
-    System.out.println("SPRING_DATASOURCE_URL=" + System.getenv("SPRING_DATASOURCE_URL"));
-    System.out.println("SPRING_DATASOURCE_USERNAME=" + System.getenv("SPRING_DATASOURCE_USERNAME"));
-    System.out.println("SPRING_DATASOURCE_PASSWORD=" + System.getenv("SPRING_DATASOURCE_PASSWORD"));
-}
+        SpringApplication app = new SpringApplication(PasswordManagerApplication.class);
 
+        app.addListeners(event ->
+                System.out.println("SPRING_DATASOURCE_URL=" + System.getenv("SPRING_DATASOURCE_URL"))
+        );
+        app.run(args);
+    }
 }
