a3cece7ace28b27bb08c1ae7b250ceceb250041f
harshit khanna <168678927+ydshotsky@users.noreply.github.com>
2025-12-28 17:45:40 +0530

    Update PasswordManagerApplication.java

--- src/main/java/com/passwordManager/PasswordManagerApplication.java
+++ src/main/java/com/passwordManager/PasswordManagerApplication.java
@@ -8,4 +8,11 @@ public class PasswordManagerApplication{
     public static void main(String[] args) {
         SpringApplication.run(PasswordManagerApplication.class, args);
     }
+    @PostConstruct
+public void logEnv() {
+    System.out.println("SPRING_DATASOURCE_URL=" + System.getenv("SPRING_DATASOURCE_URL"));
+    System.out.println("SPRING_DATASOURCE_USERNAME=" + System.getenv("SPRING_DATASOURCE_USERNAME"));
+    System.out.println("SPRING_DATASOURCE_PASSWORD=" + System.getenv("SPRING_DATASOURCE_PASSWORD"));
+}
+
 }
