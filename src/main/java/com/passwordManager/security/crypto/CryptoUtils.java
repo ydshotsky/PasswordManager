00b68cbb23e182baf07223a4fa8964c54b01dfe8
harshit khanna <168678927+ydshotsky@users.noreply.github.com>
2026-01-19 14:24:08 +0530

    Update CryptoUtils.java

--- src/main/java/com/passwordManager/security/crypto/CryptoUtils.java
+++ src/main/java/com/passwordManager/security/crypto/CryptoUtils.java
@@ -5,23 +5,17 @@ import java.security.SecureRandom;
 public class CryptoUtils {
     private static final int IV_LENGTH=12;
     private static final int SALT_LENGTH = 16;
-    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final SecureRandom secureRandom;
+    public CryptoUtils() {
+        this.secureRandom = new SecureRandom();
+    }
     public static byte[] generateRandomSalt() {
-        
-        System.out.println("generating salt");
         byte[] salt = new byte[SALT_LENGTH];
-        try{
-        secureRandom.reseed();
         secureRandom.nextBytes(salt);}
-        catch(Exception e){
-            
-        System.out.println(e);
-        }
         return salt;
     }
     public static byte[] generateIV() {
         byte[] iv = new byte[IV_LENGTH];
-        secureRandom.reseed();
         secureRandom.nextBytes(iv);
         return iv;
     }
